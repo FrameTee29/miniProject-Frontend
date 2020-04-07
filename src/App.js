11f1e74609b12618fd19c2de97e8e5748375c3f7
FrameTee29 <teeraphat123@hotmail.com>
2020-04-07 15:11:57 +0700

    Add SDK firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBAd8DV8Ubhk_1It-N8U4pZVPUlnzeEXxk",
+  authDomain: "miniprojectclient.firebaseapp.com",
+  databaseURL: "https://miniprojectclient.firebaseio.com",
+  projectId: "miniprojectclient",
+  storageBucket: "miniprojectclient.appspot.com",
+  messagingSenderId: "665443678463",
+  appId: "1:665443678463:web:a2cadb4439ac6ccae134ca",
+  measurementId: "G-Q0LLYV99YY"
+};
+
+// Instantiate a Firebase app.
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 function App() {
   return (
     <div className="App">
