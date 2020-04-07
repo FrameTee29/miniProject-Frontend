982560a74c79a1d388d78395c783ddc63cfdc9af
FrameTee29 <teeraphat123@hotmail.com>
2020-04-07 16:05:12 +0700

    Manage components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import firebaseConfig from './lib/config'
+
 import firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-
 import Signin from './components/signin';
-
-
-
-// Instantiate a Firebase app.
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 
 const App = () => {
 
-  const [isSignedIn, setIsSignedIn] = useState('');
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(
-      (user) => setIsSignedIn(!!user)
-    );
-  })
-  
-  if (!isSignedIn) {
-    return (
-      <div>
-        <Signin/>
-      </div>
-    )
-  }
-
-
   return (
     <div className="App">
-      <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
-      <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL} />
-      <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+      <Signin/>
     </div>
   );
 }
--- src/components/signin.js
+++ src/components/signin.js
@@ -2,16 +2,37 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import uiConfig from '../lib/uiConfig'
+import firebaseConfig from '../lib/config'
 
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-const Signin=()=>{
-    return(
-        <div>
-        <h1>Activity in PSU Phuket campus</h1>
-        <p>Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-      </div>
-    );
+const Signin = () => {
+
+    const [isSignedIn, setIsSignedIn] = useState('');
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(
+            (user) => setIsSignedIn(!!user)
+        );
+    })
+    if (!isSignedIn) {
+        return (
+            <div>
+                <h1>Activity in PSU Phuket campus</h1>
+                <p>Please sign-in:</p>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="App">
+                <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+                <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL} />
+                <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+            </div>
+        );
+    }
 }
 
 export default Signin;
