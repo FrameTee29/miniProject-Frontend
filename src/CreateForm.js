6395b9ad974baf72a78b4d5dc4ef443c9694c847
FrameTee29 <teeraphat123@hotmail.com>
2020-04-10 22:10:02 +0700

    onchange form

--- src/CreateForm.js
+++ src/CreateForm.js
@@ -5,7 +5,13 @@ import Topbar from './components/topbar';
 import Topbaradmin from './components/Topbaradmin';
 
 const CreateForm = () => {
+    const [name, setName] = useState('')
+    const [department, setdepartment] = useState('')
+    const [detail, setdetail] = useState('')
+    const [give, setGive] = useState('')
+    const [date, setDate] = useState('')
     const [start, setStart] = useState('')
+    const [end, setend] = useState('')
 
     if (firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th") {
         return (
@@ -15,25 +21,27 @@ const CreateForm = () => {
                     <div className="Head">CreateForm</div>
                     <form className="boxform">
                         <div className="header">ชื่อกิจกรรม</div>
-                        <input  className="input1" type="text" placeholder="ชื่อกิจกรรม..." />
+                        <input  className="input1" type="text" placeholder="ชื่อกิจกรรม..." onChange={e => setName(e.target.value)}/>
 
                         <div className="header">หน่วยงานที่จัดกิจกรรม</div>
-                        <input className="input1" type="text" placeholder="หน่วยงาน..." />
+                        <input className="input1" type="text" placeholder="หน่วยงาน..." onChange={e => setdepartment(e.target.value)}/>
 
                         <div className="header">รายละเอียด</div>
-                        <textarea className="textarea1" placeholder="Detail..." />
+                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setdetail(e.target.value)}/>
 
                         <div className="header">สิ่งที่ได้รับจากกิจกรรม</div>
-                        <textarea className="textarea1" placeholder="Detail..." />
+                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setGive(e.target.value)}/>
 
                         <div className="header">วันที่</div>
-                        <input type="date" min="2020-01-01" max="2023-12-31" />
+                        <input type="date" min="2020-01-01" max="2023-12-31" onChange={e => setDate(e.target.value)}/>
+
+
                         <div className="header">เวลาที่จัดกิจกรรม</div>
                         <div className="header1_1">เริ่มกิจกรรม</div>
                         <input type="time" onChange={e => setStart(e.target.value)} />
-                        {start}
+                    
                         <div className="header1_1">สิ้นสุดกิจกรรม</div>
-                        <input type="time" />
+                        <input type="time" onChange={e => setend(e.target.value)}/>
                         <div className="lastbutton"><button className="submit1" type="submit">Create</button></div>
                     </form>
                 </div>
