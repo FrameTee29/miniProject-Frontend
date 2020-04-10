24882f47a571ac6b67e9e1bf1a23c4d9282e6474
FrameTee29 <teeraphat123@hotmail.com>
2020-04-10 22:45:59 +0700

    next to alert success

--- src/CreateForm.js
+++ src/CreateForm.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './CreateForm.css';
-import firebase  from 'firebase';
+import firebase from 'firebase';
 import Topbar from './components/topbar';
 import Topbaradmin from './components/Topbaradmin';
+import { Route, Router } from 'react-router-dom';
 
 const CreateForm = () => {
     const [name, setName] = useState('')
@@ -15,8 +16,11 @@ const CreateForm = () => {
 
     const firestore = firebase.firestore();
 
-    const addActivity=()=>{
-        firestore.collection('Activity').doc(name).set({name,department,detail,give,date,start,end})
+    const addActivity = async () => {
+        // await firestore.collection('Activity').doc(name).set({ name, department, detail, give, date, start, end })
+        // alert("Success");
+
+        Router.push('/activity')
     }
 
     if (firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th") {
@@ -27,27 +31,27 @@ const CreateForm = () => {
                     <div className="Head">CreateForm</div>
                     <div className="boxform">
                         <div className="header">ชื่อกิจกรรม</div>
-                        <input  className="input1" type="text" placeholder="ชื่อกิจกรรม..." onChange={e => setName(e.target.value)}/>
+                        <input className="input1" type="text" placeholder="ชื่อกิจกรรม..." onChange={e => setName(e.target.value)} />
 
                         <div className="header">หน่วยงานที่จัดกิจกรรม</div>
-                        <input className="input1" type="text" placeholder="หน่วยงาน..." onChange={e => setdepartment(e.target.value)}/>
+                        <input className="input1" type="text" placeholder="หน่วยงาน..." onChange={e => setdepartment(e.target.value)} />
 
                         <div className="header">รายละเอียด</div>
-                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setdetail(e.target.value)}/>
+                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setdetail(e.target.value)} />
 
                         <div className="header">สิ่งที่ได้รับจากกิจกรรม</div>
-                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setGive(e.target.value)}/>
+                        <textarea className="textarea1" placeholder="Detail..." onChange={e => setGive(e.target.value)} />
 
                         <div className="header">วันที่</div>
-                        <input type="date" min="2020-01-01" max="2023-12-31" onChange={e => setDate(e.target.value)}/>
+                        <input type="date" min="2020-01-01" max="2023-12-31" onChange={e => setDate(e.target.value)} />
 
 
                         <div className="header">เวลาที่จัดกิจกรรม</div>
                         <div className="header1_1">เริ่มกิจกรรม</div>
                         <input type="time" onChange={e => setStart(e.target.value)} />
-                    
+
                         <div className="header1_1">สิ้นสุดกิจกรรม</div>
-                        <input type="time" onChange={e => setend(e.target.value)}/>
+                        <input type="time" onChange={e => setend(e.target.value)} />
                         <div className="lastbutton"><button className="submit1" type="submit" onClick={addActivity}>Create</button></div>
                     </div>
                 </div>
