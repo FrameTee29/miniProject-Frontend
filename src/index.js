c38dad32041b64f64c5079206b61f8b2b0035913
FrameTee29 <teeraphat123@hotmail.com>
2020-04-10 22:12:48 +0700

    set firestore in index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Router, Route, Link } from 'react-router-dom'
+import firebaseConfig from './lib/config';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+if(firebase.apps.length ===0){
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const firestore = firebase.firestore();
 
 ReactDOM.render(
   <BrowserRouter>
