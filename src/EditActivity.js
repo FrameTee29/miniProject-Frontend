9db80600e699b82d2e71f5ab5ae5fb6bd0e535f9
FrameTee29 <teeraphat123@hotmail.com>
2020-04-12 19:07:16 +0700

    getFirestore

--- src/EditActivity.js
+++ src/EditActivity.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import firebase from 'firebase';
-import Topbar from './components/topbar';
 import Topbaradmin from './components/Topbaradmin';
 
 const EditActivity = () => {
+
+    const firestore = firebase.firestore();
+
+    const getAcitivity = () => {
+        firestore.collection("Activity").get().then((querySnapshot)=> {
+            querySnapshot.forEach((doc)=>{
+                console.log(doc.id, " => ", doc.data());
+            });
+        });
+    }
+
     if (firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th") {
         return (
             <div>
                 <Topbaradmin />
             EditActivity
+                <button onClick={getAcitivity}>Click</button>
             </div>
         );
     }
