5a0552520848b5ed58d0cd82d9d66592af71b94a
FrameTee29 <teeraphat123@hotmail.com>
2020-04-13 21:50:49 +0700

    activity  60%

--- src/components/AllDetail.js
+++ src/components/AllDetail.js
@@ -7,25 +7,25 @@ const AllDetail = (props) => {
     return (
         <div className="LayoutDetail">
             <div className="headDetail">
-                ชื่อกิจกรรม<p className="intoHead">{name}</p>
+                ชื่อกิจกรรม<p className="intoHead">- {name}</p>
             </div>
             <div className="headDetail">
-                วันที่ <p className="intoHead">{date}</p>
+                วันที่ <p className="intoHead">- {date}</p>
             </div>
             <div className="headDetail">
-                หน่วยงานที่จัดกิจกรรม <p className="intoHead">{department}</p>
+                หน่วยงานที่จัดกิจกรรม <p className="intoHead">- {department}</p>
             </div>
             <div className="headDetail">
-                รายละเอียดของกิจกรรม <p className="intoHead">{detail}</p>
+                รายละเอียดของกิจกรรม <p className="intoHead">- {detail}</p>
             </div>
             <div className="headDetail">
-                สิ่งที่ได้รับจากกิจกรรม <p className="intoHead">{give}</p>
+                สิ่งที่ได้รับจากกิจกรรม <p className="intoHead">- {give}</p>
             </div>
             <div className="headDetail">
-                เริ่มกิจกรรม <p className="intoHead">{start}</p>
+                เริ่มกิจกรรม <p className="intoHead">- {start}</p>
             </div>
             <div className="headDetail">
-                สิ้นสุดกิจกรรม <p className="intoHead">{end}</p>
+                สิ้นสุดกิจกรรม <p className="intoHead">- {end}</p>
             </div>
 
         </div>
