4335c6ba89c7a914b303d0df176c2b391032257e
FrameTee29 <teeraphat123@hotmail.com>
2020-04-13 15:58:20 +0700

    Css Edit activity

--- src/EditActivity.js
+++ src/EditActivity.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import firebase from 'firebase';
 import Topbaradmin from './components/Topbaradmin';
 import Listactivity from './components/listactivity';
+import './EditActivity.css'
 
 const EditActivity = () => {
     const [activitys, setActivitys] = useState([]);
@@ -21,20 +21,24 @@ const EditActivity = () => {
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getAcitivity();
-    },[])
+    }, [])
 
     if (firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th") {
         return (
-            <div>
+            <div >
                 <Topbaradmin />
-                    EditActivity
-                { activitys.map((activity,index)=>{
-                    return(
-                        <Listactivity key={index} activity={activity}/>
-                    )
-                })}
+                <div className="OuterEdit">
+                    <div className="ContainerEdit">
+                        <div className="HeadEdit">EditActivity</div>
+                            {activitys.map((activity, index) => {
+                        return (
+                            <Listactivity key={index} activity={activity} />
+                        )
+                    })}
+                    </div>
+                </div>
             </div>
         );
     }
