6aca5a3a216d07257fcd864e1f28ffe9bc6817aa
FrameTee29 <teeraphat123@hotmail.com>
2020-04-14 19:50:48 +0700

    button sign out

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -5,19 +5,20 @@ import uiConfig from '../lib/uiConfig';
 import firebaseConfig from '../lib/config';
 import './topbar.css';
 import { Link, } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 
 const Topbar = () => {
 
     const checkEmail = () => {
 
-        if(firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th"){
+        if (firebase.auth().currentUser.email == "s6035512080@phuket.psu.ac.th") {
             return (
                 <Link to="/editactivity">
                     <button className="btn-menu">Administrator</button>
                 </Link>
             )
         }
-        else{
+        else {
             return null;
         }
     }
@@ -25,32 +26,38 @@ const Topbar = () => {
     return (
         <div>
             <div className="boxtopbar">
-                
 
-                    <Link to="/home">
-                        <img className="PSU" src="https://www.phuket.psu.ac.th/wp-content/uploads/2019/03/cropped-PSU_PHUKET-EN.png"></img>
-                    </Link>
-                    <div className="barmenu">
+
+                <Link to="/home">
+                    <img className="PSU" src="https://www.phuket.psu.ac.th/wp-content/uploads/2019/03/cropped-PSU_PHUKET-EN.png"></img>
+                </Link>
+                <div className="barmenu">
                     <Link to="/home">
                         <button className="btn-menu">Home</button>
+                    </Link>
+                    <Link to="/history">
+                        <button className="btn-menu">About</button>
                     </Link>
                     <Link to="/activity">
                         <button className="btn-menu">Activity</button>
                     </Link>
-                    <Link to="/history">
-                        <button className="btn-menu">History</button>
-                    </Link>
+
                     {checkEmail()}
-                    
+
                 </div>
                 <div className="barname">
                     <img className="PhotoURL" id="photo" src={firebase.auth().currentUser.photoURL} />
                     <div className="displayName">{firebase.auth().currentUser.displayName}</div>
-                    <button className="btn-signout" onClick={() => firebase.auth().signOut()}>Sign-out</button>
+                    <Button variant="contained" color="secondary" onClick={() => firebase.auth().signOut()}>Sign-out</Button>
                 </div>
             </div>
         </div>
     );
 }
+
+
+
+
+
 
 export default Topbar;
