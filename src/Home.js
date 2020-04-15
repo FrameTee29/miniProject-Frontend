d581a24608474e1694302fd2b86188de83d2093c
FrameTee29 <teeraphat123@hotmail.com>
2020-04-15 14:43:10 +0700

    home

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import firebase from 'firebase';
 import Topbar from './components/topbar';
 import Zoomout from './components/zoomout';
 import Part2Home from './components/Part2Home';
-
-
 
 
 const Home = () => {
